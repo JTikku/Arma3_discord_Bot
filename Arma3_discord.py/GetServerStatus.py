af6703e71039053b70633a313b3d67c4bfa0616c
JTikku <91213566+JTikku@users.noreply.github.com>
2022-03-16 10:21:57 +0200

    Add files via upload

---
+++ Arma3_discord.py/GetServerStatus.py
@@ -0,0 +1,190 @@
+import discord
+import asyncio
+import os
+from discord.ext import commands
+from discord.utils import get
+from valve.source.a2s import ServerQuerier
+from valve.source.master_server import MasterServerQuerier
+from valve.source.messages import BrokenMessageError
+from datetime import datetime
+import os.path
+
+# keepLooping = true
+# port needs to be Query Port
+address = ("157.90.34.159", 2383)
+
+PingMessage = None
+embedMessage = None
+Curchannel = None
+
+PingMessageID = None
+embedMessageID = None
+ChannelID = None
+
+pinged = "Servustatus-ping"
+#move to file getserverstatus?
+# Steamquarry loop
+
+async def Steamquarry(client, chan, ping):
+    global Curchannel
+    global pinged
+    global ChannelID
+    global embedMessageID
+    global PingMessageID
+    
+    #global keepLooping
+    keepLooping = True
+    
+    # re add ping func
+    #pinged = discord.utils.get(ctx.guild.roles,name=ping)
+    #Curchannel = client.get_channel(ChannelID)
+    
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    rel_path = "GorillaData.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    if os.path.isfile(abs_file_path):
+        f = open(abs_file_path, "r")
+        id1 = f.read()
+        f.read()
+        f.close()
+        
+        if not (not id1):
+            gorilladata = id1.split(",")
+            
+            ChannelID = int(gorilladata[0])
+            embedMessageID = int(gorilladata[1])
+            PingMessageID = None
+            
+            if (len(gorilladata) > 2):
+                PingMessageID = int(gorilladata[2])
+
+    if chan is None:
+        Curchannel = client.get_channel(ChannelID)
+    else:
+        Curchannel = discord.utils.get(client.get_all_channels(), name=chan)
+        
+    #Curchannel = discord.utils.get(ctx.guild.channels, name=chan)
+    while keepLooping:
+        try:
+            #await get_server_stats(ctx, address, chan)
+            message = await ReturnServerStatus(client, chan, Curchannel, pinged)
+        except Exception as e:
+            #print(e)
+            await asyncio.sleep(60)
+            continue
+        # except:
+            # print("Uknown exception")
+            # continue
+        # await asyncio.sleep(90)
+
+async def clearServerstatus(client):
+    limit=10
+    global Curchannel
+    global embedMessageID
+    global PingMessageID
+    
+    #msgnum = 0
+    async for m in Curchannel.history():
+        if 0 == limit:
+            break
+        if (m.author == client.user) and ((m.id != embedMessageID) and (m.id != PingMessageID)):
+            await m.delete()
+            limit = limit - 1
+    #await Curchannel.delete_messages(msg)
+
+# Clear up code
+# Console messages are overlapping
+async def ReturnServerStatus(client, chan, Curchannel, pinged):
+        now = datetime.now()
+        global address
+        global embedMessage
+        
+        # global Curchannel
+        # global pinged
+        global embedMessageID
+        global PingMessageID
+        
+        try:
+            server = ServerQuerier(address, timeout=15.0)
+
+            print(f"Done: {now.strftime('%H:%M')}", end = "\r")
+            
+            embed=discord.Embed(title= server.info()["server_name"])
+            embed.set_footer(text= f"{now.strftime('%d.%m.%Y %H:%M')}")
+            embed.add_field(name= "Status", value= "Online", inline=False)
+            
+            if (server.info()["map"] != ""):
+                embed.add_field(name= "Kartta:", value= server.info()["map"], inline=False)
+            else:
+                embed.add_field(name= "Kartta:", value= "-", inline=False)
+
+            if (server.info()["game"] != ""):
+                embed.add_field(name= "Tehtävä:", value= server.info()["game"], inline=False)
+            else:
+                embed.add_field(name= "Tehtävä:", value= "-", inline=False)
+            
+            embed.add_field(name= "Pelaajamäärä:", value= f"{server.info()['player_count']}/{server.info()['max_players']}", inline=False)
+            embed.add_field(name= "\u200B", value= "\u200B", inline=True)
+            PlayerList = []
+            for player in server.players()["players"]:
+                PlayerList.append(f"{player['name']} {'{:02d}:{:02d}'.format(*divmod(round((player['duration'] / 60)), 60))}")
+
+            if (len(PlayerList) > 0):
+                playernames = '\n'.join(PlayerList)
+                embed.add_field(name= "Pelaajalista:", value= f"```{playernames}```", inline=False)
+            else:
+                embed.add_field(name= "Pelaajalista:", value= "```Ei pelaajia```", inline=False)
+        # except valve.source.NoResponseError:
+            # server.close()
+            # print("NoResponseError")
+            # raise
+        except Exception as e:
+            print(f"{now.strftime('%H:%M')}: {e}", end = "\r")
+            # print(e)
+            server.close()
+            embed=discord.Embed(title= "Server didn't respond")
+            embed.add_field(name= "Status:", value= "Offline", inline=False)
+            embed.set_footer(text= f"{now.strftime('%d.%m.%Y %H:%M')}")
+            
+            embedMessage = discord.utils.get(await Curchannel.history(limit=100).flatten() , id = embedMessageID)
+            if embedMessage is None:
+                await clearServerstatus(client)
+                embedMessage = await Curchannel.send(embed=embed)
+            else:
+                await embedMessage.edit(embed=embed)
+            embedMessageID = embedMessage.id
+            
+            raise  
+        
+        embedMessage = discord.utils.get(await Curchannel.history(limit=100).flatten() , id = embedMessageID)
+        # print(embedMessage)
+        if embedMessage is None:
+            await clearServerstatus(client)
+            embedMessage = await Curchannel.send(embed=embed)
+        else:
+            await embedMessage.edit(embed=embed)
+        embedMessageID = embedMessage.id
+        
+        global PingMessage
+        if (len(PlayerList) > 3):
+            if PingMessage is None:
+                PingMessage = await Curchannel.send(f"{pinged.mention} 4 Pelaajan raja on täyttynyt, hyppää palvelimelle!")
+        elif PingMessageID is not None:
+            PingMessage = discord.utils.get(await Curchannel.history(limit=100).flatten() , id = PingMessageID)
+            await PingMessage.delete()
+            PingMessage = None
+        
+        # add check if noting has changed
+        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+        rel_path = "GorillaData.txt"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        f = open(abs_file_path, "w")
+        
+        # save ping message if message was posted
+        if (PingMessage is None):
+            f.write(f"{Curchannel.id},{embedMessage.id}")
+        else:
+            f.write(f"{Curchannel.id},{embedMessage.id},{PingMessage.id}")
+        f.close()
+        return embedMessage
---
+++ Arma3_discord.py/GorillaBot.py
@@ -0,0 +1,131 @@
+import discord
+import asyncio
+import os
+from discord.ext import commands
+from discord.utils import get
+from datetime import datetime
+import GetServerStatus
+
+client = commands.Bot(command_prefix = "?")
+
+QuarryLoop = None
+# ChannelID = None
+
+#pinged = "Servustatus-ping"
+defaultPinged = "Servustatus-ping"
+servustatus = "servustatus"
+ehdotukset = "ehdotukset"
+
+@client.event
+async def on_ready():
+    print("GorillaBot is ready.")
+    await ServerStatus(client)
+
+@client.event
+async def on_message(message):
+    if(((str(message.channel)).split()[0] != "Direct") and str(message.channel.name) == ehdotukset):
+        await message.add_reaction("\U0001F44D")
+        await message.add_reaction("\U0001F44E")
+    await client.process_commands(message)
+
+#########################    Client Commands   ######################
+
+#ServerStatus
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def ServerStatus(ctx, chan = servustatus, pinged = defaultPinged):
+    print("Start Steam Quarry")
+    Bot = client
+    global QuarryLoop
+    # client.loop.create_task(GetServerStatus.Steamquarry(client, chan, pinged))
+    QuarryLoop = client.loop.create_task(GetServerStatus.Steamquarry(Bot, chan, pinged))
+
+# maintenancemode
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def maintenancemode(ctx):
+    #await ctx.message.delete()
+    
+    #global PingMessage
+    global QuarryLoop
+    
+    QuarryLoop.cancel()
+    QuarryLoop = None
+    
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    rel_path = "GorillaData.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    if os.path.isfile(abs_file_path):
+        f = open(abs_file_path, "r")
+        id1 = f.read()
+        f.read()
+        f.close()
+        
+        if not (not id1):
+            #ChannelID, embedMessageID = id1.split(",")
+            gorilladata = id1.split(",")
+
+            ChannelID = int(gorilladata[0])
+            embedMessageID = int(gorilladata[1])
+            
+            Curchannel = client.get_channel(ChannelID)
+            embedMessage = discord.utils.get(await Curchannel.history(limit=100).flatten() , id = embedMessageID)
+            await embedMessage.delete()
+            
+            if (len(gorilladata) > 2):
+                PingMessageID = int(gorilladata[2])
+                PingMessage = discord.utils.get(await Curchannel.history(limit=100).flatten() , id = PingMessageID)
+                await PingMessage.delete()
+
+    await Curchannel.send("Under Maintenance")
+    print("Under Maintenance")
+
+# ResetServerStatus
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def ResetServerStatus(ctx, chan = servustatus, pinged = defaultPinged):
+    await clearbotmessages(ctx, 3, chan)
+    await ServerStatus(ctx, chan, pinged)
+    
+# re write
+# # ResetServerStatusID
+# @client.command()
+# @commands.has_permissions(manage_channels=True)
+# async def ResetServerStatusID(ctx, ID = embedMessageID, chan = servustatus, pinged = defaultPinged):
+    # embedMessageID = ID
+    # await ServerStatus(ctx, chan, pinged)
+
+# shutdown
+@client.command()
+@commands.has_permissions(administrator=True)
+async def shutdown(ctx):
+    await ctx.bot.logout()
+
+# clear commands
+@client.command()
+@commands.has_permissions(administrator=True)
+async def clear(ctx, channel, amount=0,):
+    if (isinstance(channel, str)):
+        channel = discord.utils.get(ctx.guild.channels, name=channel)
+    await channel.purge(limit=amount)
+
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def clearbotmessages(ctx, limit=0, chan = servustatus):
+    #await ctx.message.delete()
+    Curchannel = discord.utils.get(ctx.guild.channels, name=chan)
+    msg = []
+    async for m in Curchannel.history():
+        if len(msg) == limit:
+            break
+        if m.author == client.user:
+            msg.append(m)
+    await Curchannel.delete_messages(msg)
+    #await ctx.send(f"Purged {limit} messages of {client.user.mention}", delete_after=3)
+
+# @ServerStatus.error
+# async def ServerStatus_error(ctx, error):
+    # print("Sorry, you do not have permissions to do that!")
+    # #await ctx.send("Sorry, you do not have permissions to do that!")
+client.run("ODcyNzY1MDU2NTUzNzEzNzQ3.YQunzA.hQFGc9XW4c0nSSBr6y-gBVqfZ5w")
